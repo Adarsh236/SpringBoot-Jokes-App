0e518d81f67c3e75d1f1402a8a010ad089ff2dd6
adarshverma236 <adarshverma11236@gmail.com>
2020-02-11 18:07:44 +0200

    Spring XML Configuration. closes #2

--- jokes-app/src/main/java/adarsh/springboot/jokesapp/JokesAppApplication.java
+++ jokes-app/src/main/java/adarsh/springboot/jokesapp/JokesAppApplication.java
@@ -2,8 +2,10 @@ package adarsh.springboot.jokesapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ImportResource;
 
 @SpringBootApplication
+@ImportResource("classpath:chuck-config.xml")// xml for java config
 public class JokesAppApplication {
 
     public static void main(String[] args) {
